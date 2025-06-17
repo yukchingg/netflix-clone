f2dc8909fddcc16da347c60a400b344cdae1d87f
yukchingg <wychan636@hotmail.com>
2025-06-18 05:49:46 +1000

    Navbar Setup
    -set up the navbar and the styles

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TitleCards.css";
+
+const TitleCards = () => {
+  return <div className="title-cards"></div>;
+};
+
+export default TitleCards;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
